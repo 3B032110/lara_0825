bc89d7c454013cafb01d3e859cd5d327e7c89ebf
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2023-11-07 17:18:01 +0800

    Using the all method of Model Post to query data (all data)

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,22 @@ Route::get('/', function () {
 	// $post->content = "test content";
 	// $post->save();
 
-    // find method
-    $post = Post::find(1);
-	echo '標題: '.$post->title.'<br>';
-	echo '內容: '.$post->content.'<br>';
+    // find method (id = 1 data)
+    // $post = Post::find(1);
+	// echo '標題: '.$post->title.'<br>';
+	// echo '內容: '.$post->content.'<br>';
+	// dd($post);
+
+    // all method (all data)
+    $posts = Post::all();
+
+	// each loop(foreach) fetch one post in sequence
+	foreach($posts as $post){
+		echo '編號: '.$post->id.'<br>';
+		echo '標題: '.$post->title.'<br>';
+		echo '內容: '.$post->content.'<br>';
+		echo '張貼時間: '.$post->created_at.'<br>';
+	}
 	dd($post);
 
 	return 'Saved, OK!';
