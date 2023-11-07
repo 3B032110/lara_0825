4243b1ff03cf1faf303c04ca0d7c967fdde22de8
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2023-11-07 17:11:56 +0800

    Using the create method of Model Post to add data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    Post::create([
+		'title'=>'created title',
+		'content'=>'created content',
+	]);
 });
