de40130d6c3e01a1afdb62bd20d92925d50d02ea
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2023-11-07 17:19:22 +0800

    Using the where method of Model Post to query data (id < 10 data)

--- routes/web.php
+++ routes/web.php
@@ -15,35 +15,39 @@ use App\Models\Post;
 */
 
 Route::get('/', function () {
-    // create method
+    // // create method
     // Post::create([
 	// 	'title'=>'created title',
 	// 	'content'=>'created content',
 	// ]);
 
-    // save method 
+    // // save method 
     // $post = new Post();
 	// $post->title = "test title";
 	// $post->content = "test content";
 	// $post->save();
 
-    // find method (id = 1 data)
+    // // find method (id = 1 data)
     // $post = Post::find(1);
 	// echo '標題: '.$post->title.'<br>';
 	// echo '內容: '.$post->content.'<br>';
 	// dd($post);
 
-    // all method (all data)
-    $posts = Post::all();
+    // // all method (all data)
+    // $posts = Post::all();
 
-	// each loop(foreach) fetch one post in sequence
-	foreach($posts as $post){
-		echo '編號: '.$post->id.'<br>';
-		echo '標題: '.$post->title.'<br>';
-		echo '內容: '.$post->content.'<br>';
-		echo '張貼時間: '.$post->created_at.'<br>';
-	}
-	dd($post);
+	// // each loop(foreach) fetch one post in sequence
+	// foreach($posts as $post){
+	// 	echo '編號: '.$post->id.'<br>';
+	// 	echo '標題: '.$post->title.'<br>';
+	// 	echo '內容: '.$post->content.'<br>';
+	// 	echo '張貼時間: '.$post->created_at.'<br>';
+	// }
+	// dd($post);
+
+    // where method (id < 10)
+	$posts = Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
+	dd($posts);
 
 	return 'Saved, OK!';
 });
